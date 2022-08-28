dc0262c8cd3b4db15307f810dbd5ecdae5b22639
Arafat Mohamed <arafatmohamed@Arafats-MacBook-Pro.local>
2022-08-28 21:22:30 +0100

    arrays

---
+++ syntax.js
@@ -0,0 +1,27 @@
+
+
+//print a number
+console.log (2)
+
+//print a string
+
+console.log ("tomorrow")
+
+//creating a variable and redeclaring it by printing both variables
+
+let name = "Hiba"
+console.log (name)
+
+
+name= "hassan"
+console.log (name)
+
+//making a constant variable
+
+const number = 2
+
+console.log (number)
+
+//Array
+let array =[1, 2, 3, 4, 5, 6, 7, 8, 9 ,10, 11, 12, 13, 14, 15, "green", "blue"] 
+console.log(array)
