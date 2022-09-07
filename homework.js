6e14dda59bb9519e5fd301522a994a1900c4f49b
Arafat Mohamed <arafatmohamed@Arafats-MacBook-Pro.local>
2022-09-07 19:49:22 +0100

    conditionals

---
+++ homework.js
@@ -0,0 +1,20 @@
+
+var gender = "girl"
+var dob = 2014
+var year = 2024
+
+if (gender === "boy" && dob === 2011) {
+console.log ("this is my eldest hudhayfah")
+
+} else if  ((gender != "boy") && dob === 2016) 
+
+{ console.log ("She is my youngest Maysarah")
+
+}else if ((gender != "boy") && dob === 2014){
+    console.log ("Middle child Suhayla")
+} else if ((year > 2023)){
+    console.log ("you dont exist")
+}else {
+    console.log ("neither of them are my children")
+}
+
