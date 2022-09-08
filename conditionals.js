358969328c4eba8d89ba86e4447b7eb260acd6e2
Arafat Mohamed <arafatmohamed@Arafats-MacBook-Pro.local>
2022-09-08 18:42:23 +0100

    conditionals homework

---
+++ conditionals.js
@@ -0,0 +1,32 @@
+var name = "Hiba"
+var gender = "female"
+var surname= "hassan"
+
+
+if (name === "Hiba" && gender === "female" && surname === "hassan"){
+    console.log ("This is the eldest daughter hiba")
+}else if (name === "Abdulghani" && gender != "female " && surname ==="hassan"){
+    console.log ("This is the eldest son")
+
+}else if (name === "Naima" && gender === "female" && surname === "hassan"){
+    console.log ("This is the second eldest daugher Naima")
+
+}else if (name === "Nasra" && gender === "female" && surname === "hassan"){
+    console.log ("This is my sister nasra")
+}else if (name === "Nasteha" && gender === "female" && surname === "hassan"){
+    console.log ("this is my third youngest")
+}else if (name === "Fadli" && gender != "female" && surname === "hassan"){
+    console.log ("this is the youngest boy")
+}else if (name === "Zaynab" && gender === "female" && surname === "hassan"){
+    console.log ("This is my favourite sister")
+}else if (name === "Hudhayfah" && gender != "female" && surname === "hassan"){
+    console.log ("eldest grandchild Hudhayfah")
+}else if (name === "Suhayla" && gender === "female" && surname === "mohamed"){
+    console.log ("first granddaughter Suhayla")
+}else if (name === "Maysarah" && gender === "female" && surname === "mohamed"){
+    console.log ("youngest granddaughter Maysarah")
+} else (name === "Ayaan" && gender === female && surname === "diesow")
+    console.log ("She wishes she was a hassan")
+
+
+
