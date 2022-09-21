a85c47c367cd282edc6a09a57cc41b90bf945aa2
Arafat Mohamed <arafatmohamed@Arafats-MacBook-Pro.local>
2022-09-21 09:59:22 +0100

    function meals

---
+++ functionhw.js
@@ -0,0 +1,34 @@
+var person = "hudhayfah"
+person = person.toString () .toLowerCase()
+var meal = "lasange" 
+meal = meal.toString () .toLowerCase()
+
+/*function familyMember (person){
+    let person = "mum"
+   if (person === "mum"){
+   } else if (person === "dad"){
+   } else if (person != "mum" | "dad"){
+     console.log ("not my parents")
+   }
+} 
+
+
+function recipe (meal){
+    let meal = "bariis"
+    if (meal === "bariis"){
+    } else if (meal === "chicken"){
+    } else if (meal === "hilib"){
+    }else{  (meal != "bariis"| "chicken" | "hilib")
+      console.log ("not on the menu")
+    } 
+}*/
+
+function dinner (person,meal){
+    if (person === "mum" && meal === "bariis"){
+        console.log ("mums favourite is bariis")
+    }else if (person === "hudhayfah" && meal === "lasange"){
+        console.log ("Hudhayfah's favourite meal is lasagne")
+    }else ("i aint got a clue")
+}
+
+dinner (person, meal)
