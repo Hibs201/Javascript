a0ab7f2ee04ca9546c1114a99a394f3baace77cc
Arafat Mohamed <arafatmohamed@Arafats-MacBook-Pro.local>
2022-09-26 12:31:28 +0100

    loopshw

---
+++ loops.js
@@ -0,0 +1,51 @@
+/*for (let countDown= 10; countDown>=0; countDown --){
+console.log (countDown)
+
+}
+
+
+for (let integer =-100; integer<=1500; integer ++){
+    console.log (integer)
+}
+
+
+for (let hiba =0; hiba <= 500; hiba++ ){
+    console.log (hiba)
+}
+
+var makeup = ["blusher", "lipstick", "eyeshadow", "mascara", "primer", "bronzer", "contour", "lipgloss", "eyeliner", "highlighter"]
+
+
+
+for (let makeup =0; makeup<list.length; makeup++){
+    console.log (list[10])
+}
+*/
+
+
+let num=10
+
+
+while (num>=0){
+    console.log (num)
+    num --
+}
+
+let countDown =50
+while(countDown>=-50){
+    console.log(countDown)
+    countDown --
+
+}
+
+let integer = -100
+while (integer<=1500){
+    console.log (integer)
+    integer ++
+}
+
+let countUp = 0
+while (countUp<=500){
+console.log (countUp)
+countUp ++
+}
